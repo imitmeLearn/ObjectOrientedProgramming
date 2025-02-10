2371822409fe5902e3d2f4261b583abbb93f281b
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-10 16:58:48 +0900

    2.FireStation - FireChief - 다형성

--- 2.FireStation/Main.cpp
+++ 2.FireStation/Main.cpp
@@ -13,6 +13,10 @@ int main()
 
 	harry->ExtinguishFire();	//harry->TellFirefighterToExtinguishFire(ronnie);	//해리가 불 안끄고, 로니가 끔.위임받았기에
 
+	//다형성
+	Firefighter* stillHarry = harry;
+	stillHarry -> ExtinguishFire();
+
 	truckOne->SetDriver(ronnie);
 
 	james->Drive(truckOne,Point(20,-3)); //운전자가 달라서 pass 되는 부분
