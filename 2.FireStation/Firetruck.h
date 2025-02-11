cd9bc86f84c36603be407226e1a31f9c0cd22ee9
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-11 12:20:21 +0900

    질문2) 전방선언으로 포인터는 안돼, 순회때문이라고 했는데, 개념적인 이야기가 아니가, 시스템적? 컴파일? 물리적인 거야??

--- 2.FireStation/Firetruck.h
+++ 2.FireStation/Firetruck.h
@@ -4,8 +4,6 @@
 #include "Ladder.h"
 #include "Hose.h"
 
-//#include "Firefighter.h"//전방선언 순환참조
-
 class Firetruck
 {
 public:
@@ -41,13 +39,11 @@ public:
 	}
 
 	class Firefighter* GetDriver() const
-		//Firefighter* GetDriver() const
 	{
 		return driver;
 	}
 
 	void SetDriver(class Firefighter* driver)
-		//void SetDriver(Firefighter* driver)
 	{
 		this -> driver = driver;
 	}
@@ -57,3 +53,67 @@ private:
 	Ladder* ladder = nullptr;
 	Hose* hose = nullptr;
 };
+
+#pragma region Q1
+//#pragma once
+//#include <iostream>
+//#include "Point.h"
+//#include "Ladder.h"
+//#include "Hose.h"
+//
+//#include "Firefighter.h"//전방선언 순환참조
+//
+//class Firetruck
+//{
+//public:
+//	Firetruck()
+//	{
+//		ladder = new Ladder(10.f);
+//	}
+//	~Firetruck()
+//	{
+//		delete ladder;	//생성주기 직접 관리
+//	}
+//
+//	//이동
+//	void Drive(const Point& position)
+//	{
+//		if(driver == nullptr)
+//		{
+//			return;
+//		}
+//		std::cout<<position<<"위치로 소방차 이동 중 \n";
+//	}
+//	const Ladder* GetLadder() const
+//	{
+//		return ladder;
+//	}
+//	const Hose* GetHose() const
+//	{
+//		return hose;
+//	}
+//	void SetHose(Hose* hose)
+//	{
+//		this->hose = hose;
+//	}
+//
+//	//class Firefighter* GetDriver() const
+//	Firefighter* GetDriver() const
+//	{
+//		return driver;
+//	}
+//
+//	//void SetDriver(class Firefighter* driver)
+//	void SetDriver(Firefighter* driver)
+//	{
+//		this -> driver = driver;
+//	}
+//
+//private:
+//	Firefighter* driver = nullptr; //전방선언 순환참조
+//	//class	Firefighter* driver = nullptr; //전방선언 순환참조	//Firefighter* driver = nullptr; //전방선언 순환참조
+//	Ladder* ladder = nullptr;
+//	Hose* hose = nullptr;
+//};
+
+#pragma endregion
