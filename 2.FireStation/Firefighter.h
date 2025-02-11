c69363a8185131aa99242c7fdcfec7dfd3069ca2
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-11 14:27:27 +0900

    2.FireStation - Firefighter - 내부 메소드 확장
    protected:	//확장할 수 있으니 protected? → 실제 그 상황이 왔을 때 확장해야지, 현상에서 미리 하는것은, 좋은 것 아니다.  →  이유가 다 있어. 필요할 때 해도 늦지 않는다!!!protected:	//확장할 수 있으니 protected? → 실제 그 상황이 왔을 때 확장해야지, 현상에서 미리 하는것은, 좋은 것 아니다.  →  이유가 다 있어. 필요할 때 해도 늦지 않는다!!!

--- 2.FireStation/Firefighter.h
+++ 2.FireStation/Firefighter.h
@@ -43,15 +43,15 @@ public:
 		this->name =name;
 	}
 
-private:
+protected:	//확장할 수 있으니 protected? 실제 그 상황이 왔을 때 확장해야지, 현상에서 미리 하는것은, 좋은 것 아니다.
 	//호스 켜기(물)
-	void TurnOnHose()
+	virtual	void TurnOnHose()
 	{
 		std::cout<<name<<"소방관! 불이 꺼지고 있습니다. "<<"\n";
 	}
 
 	//호스 조준.
-	void TrainHoseOnFire()
+	virtual	void TrainHoseOnFire()
 	{
 		std::cout<<name<<"소방관! 호스를 불이 발생한 곳에 겨냥하고 있습니다."<<"\n";
 	}
